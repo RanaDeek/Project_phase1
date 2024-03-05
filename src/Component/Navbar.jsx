0af97adacac42a2f5fcf4dac996c31a507014eb1
Rana Deek <ranadeek2002@gmail.com>
2024-03-06 01:01:04 +0200

    Second commit

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import { Link , NavLink} from 'react-router-dom'
 import './Navbar.css'
+import pic from "../assets/1.png"
 function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
-                <Link className="navbar-brand" to='/' >Home</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon" />
-                </button>
+                <img src={pic} alt="Logo" className="image" />
+                <Link className="navbar_name" to='/' >Fashion Shop</Link>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
                         <li className="nav-item">
                             <NavLink className="nav-link" to="/Products">Products</NavLink>
                         </li>
@@ -23,11 +22,11 @@ function Navbar() {
 
                     </ul>
                     <ul className="d-flex list-unstyled">
-                        <li className="nav-item me-2 mb-lg-0">
+                        <li className="nav-item1">
                             <Link className='nav-link text-decoration-none' to="/SignIn">SignIn</Link>
                         </li>
-                        <li className="nav-item">
-                            <Link className='nav-link text-primary text-decoration-underline' to="/SignUp">SignUp?</Link>
+                        <li className="nav-item1">
+                            <Link className='nav-link text-decoration-underline' to="/SignUp">SignUp?</Link>
                         </li>
                     </ul>
 
